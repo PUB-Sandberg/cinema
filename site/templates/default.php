d5fe149f956507d6c12b1b697be7061b02357915
saschakrischock <s.krischock@gmail.com>
2020-04-02 16:57:40 +0200

    add kirby

---
+++ site/templates/default.php
@@ -0,0 +1,39 @@
+
+<div class="stage">
+<?php 
+// using the `toStructure()` method, we create a structure collection
+$items = $site->movies()->toStructure();
+// we can then loop through the entries and render the individual fields
+foreach ($items as $item): ?>
+
+<?php  if ($item->category() == 'vimeo') : ?>
+  <div class='embed-container'>
+    <iframe src='https://player.vimeo.com/video/66140585' frameborder='0' webkitAllowFullScreen mozallowfullscreen allowFullScreen></iframe>
+  </div>
+<?php endif ?>
+
+<?php  if ($item->category() == 'youtube') : ?>
+    hello 
+<?php endif ?>
+
+<?php  if ($item->category() == 'jitsi') : ?>
+    hello 
+<?php endif ?>
+  <h2><?= $item->title()->html() ?></h2>
+  <h2><?= $item->url()->html() ?></h2>
+  <h2><?= $item->date()->html() ?></h2>
+<?php endforeach ?>
+</div>
+
+
+<div class="timeline">
+<?php 
+// using the `toStructure()` method, we create a structure collection
+$items = $site->movies()->toStructure();
+// we can then loop through the entries and render the individual fields
+foreach ($items as $item): ?>
+  <h2><?= $item->title()->html() ?></h2>
+  <h2><?= $item->date()->html() ?></h2>
+<?php endforeach ?>
+</div>
+
