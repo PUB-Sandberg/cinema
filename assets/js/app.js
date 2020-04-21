648b238bd646807179b8e796f0290a9e7c64838f
saschakrischock <s.krischock@gmail.com>
2020-04-21 17:40:34 +0200

    latest changes

--- assets/js/app.js
+++ assets/js/app.js
@@ -4,4 +4,16 @@ $( document ).ready(function() {
           $(this).toggleClass("active");
 
       });
+
+      $(".popcorn").click(function(){
+        $("#particles-js").toggleClass("active");
+        particlesJS.load('particles-js', 'assets/js/particles.json', function() {
+            console.log('callback - particles.js config loaded');
+          });
+
+    });
+
+    $(".curtain").click(function(){
+        $(".curtain-left, .curtain-right").toggleClass("active");
+    });
 });
